d1d12ed7f3433b68e01874fd250c8421ad9f600f
Burak <cccswagboyyolo@yahoo.com>
2021-07-15 20:51:15 +0300

    feat: add label component

---
+++ src/components/Label/index.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@test";
+
+import { Label } from "./index";
+
+describe("Rectangle button", () => {
+    it("renders without crashing", () => {
+        const component = render(<Label text="" />);
+
+        expect(component).toBeTruthy();
+    });
+    it("displays text", () => {
+        const mockText = "test";
+        render(<Label text={mockText} />);
+
+        expect(screen.getByText(mockText)).toBeDefined();
+    });
+    it("displays children", () => {
+        const mockChild = <div data-testid="mockChild" />;
+        render(<Label text="">{mockChild}</Label>);
+
+        expect(screen.getByTestId("mockChild")).toBeTruthy();
+    });
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { CircleButton } from "./CircleButton";
 export { Container } from "./Container";
 export { RectangleButton } from "./RectangleButton";
+export { Label } from "./Label";
