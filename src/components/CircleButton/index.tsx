b0c5b0663c18d60252fe08afeda97918e3726c19
Burak <cccswagboyyolo@yahoo.com>
2021-07-15 21:02:06 +0300

    refactor(component): set children of circle button to middle

--- src/components/CircleButton/index.tsx
+++ src/components/CircleButton/index.tsx
@@ -18,7 +18,7 @@ export const CircleButton: React.FC<IButton> = ({
             onClick={onClick}
             className={cn(
                 styles.button,
-                `rounded-full w-14 h-14 bg-white flex hover:no ${className}`,
+                `rounded-full w-14 h-14 bg-white justify-center items-center flex hover:no ${className}`,
             )}
             {...rest}
             data-testid="circle-button"
