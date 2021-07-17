54df7273e7857c233f4cfaacfb6e3aa3fc048a41
Burak <cccswagboyyolo@yahoo.com>
2021-07-17 22:58:47 +0300

    refactor: move game type to types/

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    type Question = { answer: string; question: string };
-    type Game = {
-        name: string;
-        time: number;
-        questions: question[];
-    };
-}
