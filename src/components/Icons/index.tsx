38d9e2b85531fd7263394d2c4123a3ab3d71beba
Burak <cccswagboyyolo@yahoo.com>
2021-07-17 22:35:02 +0300

    feat(icon): add ArrowUp and Save icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,5 +1,7 @@
 export { default as ArrowBottom } from "./ArrowBottom";
+export { default as ArrowUp } from "./ArrowUp";
 export { default as ChevronLeft } from "./ChevronLeft";
 export { default as ChevronRight } from "./ChevronRight";
 export { default as Navigation } from "./Navigation";
 export { default as Plus } from "./Plus";
+export { default as Save } from "./Save";
